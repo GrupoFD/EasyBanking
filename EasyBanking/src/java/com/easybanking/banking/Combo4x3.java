409098c4b1e6e13b7d87a2a113eb9a995824dcdd
GlennBenavides <glenn.benavides.28.agosto@gmail.com>
2015-08-16 14:03:22 -0600

    CombosNuevos 1

--- EasyBanking/src/java/com/easybanking/banking/Combo4x3.java
+++ EasyBanking/src/java/com/easybanking/banking/Combo4x3.java
@@ -9,61 +9,28 @@ package com.easybanking.banking;
  *
  * @author Glenn Benavides
  */
-public class Combo4x3 {
+public class Combo4x3 extends Combos {
+
+    private final double SPECIAL_GIFT;
+    // El COMBO4x3 le regala una cuenta a un cliente, la número 4,
+    // con la condición de que el cliente haga 3 cuentas de ahorro,
+    // una en cada moneda..!!
+
+    public Combo4x3(double SPECIAL_GIFT) {
+        this.SPECIAL_GIFT = SPECIAL_GIFT;
+    }
+   
     
-    private BankAccount account_1;
-    private BankAccount account_2;
-    private BankAccount account_3;
-    private BankAccount account_4;
 
-    public Combo4x3() {
+    public Combo4x3(BankAccount account_Colones, BankAccount account_Dollars, BankAccount account_Euros, BankAccount account_Credit) {
+        super(account_Colones, account_Dollars, account_Euros, account_Credit);
+        this.SPECIAL_GIFT = 100;
     }
 
-    public Combo4x3(BankAccount account_1, BankAccount account_2, BankAccount account_3, BankAccount account_4) {
-        this.account_1 = account_1;
-        this.account_2 = account_2;
-        this.account_3 = account_3;
-        this.account_4 = account_4;
+    public double getSPECIAL_GIFT() {
+        return SPECIAL_GIFT;
     }
 
-    public BankAccount getAccount_1() {
-        return account_1;
-    }
 
-    public void setAccount_1(BankAccount account_1) {
-        this.account_1 = account_1;
-    }
-
-    public BankAccount getAccount_2() {
-        return account_2;
-    }
-
-    public void setAccount_2(BankAccount account_2) {
-        this.account_2 = account_2;
-    }
-
-    public BankAccount getAccount_3() {
-        return account_3;
-    }
-
-    public void setAccount_3(BankAccount account_3) {
-        this.account_3 = account_3;
-    }
-
-    public BankAccount getAccount_4() {
-        return account_4;
-    }
-
-    public void setAccount_4(BankAccount account_4) {
-        this.account_4 = account_4;
-    }
     
-    public void applyComboToClient(Person client, BankAccount comboAccount_1, BankAccount comboAccount_2, BankAccount comboAccount_3, BankAccount comboAccount_4) {
-    
-        client.addAccount(account_1);
-        client.addAccount(account_2);
-        client.addAccount(account_3);
-        client.addAccount(account_4);
-    
-    }
 }
