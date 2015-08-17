e2c20202acea776df6b876ac599913995e67b1da
joss185 <joselyn_rg@outlook.com>
2015-08-17 16:42:28 -0600

    Glenn agregó un detalle... desde Joss-compu..222

--- EasyBanking/src/java/com/easybanking/banking/Transfer.java
+++ EasyBanking/src/java/com/easybanking/banking/Transfer.java
@@ -9,78 +9,66 @@ import java.util.Calendar;
 
 /**
  *
- * @author Soler
+ * @author Glenn Benavides
  */
 public class Transfer extends Transaction {
 
     private double transferToUser;
+    private String beneficiaryOfTransaction;
 
     public Transfer(double transferToUser, Calendar timeStamp, double amountOfTransaction) {
         super(timeStamp, amountOfTransaction);
         this.transferToUser = transferToUser;
     }
+    
+    public String getBeneficiaryOfTransaction() {
+        return this.beneficiaryOfTransaction;
+    }
 
     public void transferAmount(Person person, BankAccount bankAccount, Person personToTransfer, BankAccount bankAccountTotransfer) {
 
         for (BankAccount b : person.getlistOfBankAccounts()) {
-
             while (b.equals(bankAccount)) {
-
                 if (this.verifyPersonToTransferAccount(personToTransfer, bankAccountTotransfer)) {
-
                     if (this.verifyAccountCurrency(bankAccount, bankAccountTotransfer)) {
-
                         double amount = b.getAmount();
-
                         if (amount >= getAmountOfTransaction()) {
 
                             double finalAmount = amount - this.getAmountOfTransaction();
                             b.setAmount(finalAmount);
-
-                            double finalAmount2 = bankAccountTotransfer.getAmount() + this.getAmountOfTransaction();
-                            bankAccountTotransfer.setAmount(finalAmount2);
+                            this.beneficiaryOfTransaction = personToTransfer.getName();
                             
                         } else {
-
                             System.out.println("EXCEPTION");
-
                         }
-
                     }
-
                 }
-
             }
         }
     }
+
 
     public boolean verifyPersonToTransferAccount(Person personToTransfer, BankAccount bankAccountTotransfer) {
 
         for (BankAccount b : personToTransfer.getlistOfBankAccounts()) {
-
             if (b.equals(bankAccountTotransfer)) {
-
                 return true;
-
             } else {
-
                 System.out.println("EXEPTION");
-
             }
-
         }
         return false;
     }
 
+
     public boolean verifyAccountCurrency(BankAccount bankAccount, BankAccount bankAccountTotransfer) {
 
         if (bankAccount.getCurrency() == bankAccountTotransfer.getCurrency()) {
-
             return true;
         } else {
-
             System.out.println("EXCEPTION");
             return false;
         }
     }
+    
 }
